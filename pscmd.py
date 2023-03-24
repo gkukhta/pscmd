4d0c53dfba06f8d6038998432e586637cba2e70c
Gleb Kukhta <kukhta@fintech.ru>
2023-03-24 06:23:14 +0300

    match

--- pscmd.py
+++ pscmd.py
@@ -6,6 +6,8 @@ import json
 import threading
 import time
 import psutil
+import subprocess
+import os
 
 DEBUG = True
 
@@ -22,14 +24,15 @@ kmd_examples['cmd_start'] = {
     'ps':
         [
             {
-                'cmd': ['/usr/bin/ps', '-ax'],
-                'env': [
-                    ('LD_LIBRARY_PATH', '/opt/mylib')
-                ],
-                'cwd': '/tmp'
+                'cmd': ['/usr/bin/dd', 'if=/dev/zero', 'of=/dev/null']
             },
             {
-                'cmd': ['/usr/bin/dd', 'if=/dev/zero', 'of=/dev/null']
+                'cmd': ['/usr/bin/pss', '-ax'],
+                'env': {
+                    'LD_LIBRARY_PATH': '/opt/mylib',
+                    'SOME_PARAMETER': 'value-x'
+                },
+                'cwd': '/tmp'
             }
         ],
     'id': 'de14-8822'
@@ -51,90 +54,112 @@ kmd_examples['cmd_test'] = {
 }
 
 
-def glavnaya():
-    cmd_queue = Queue()
-    reply_queue = Queue()
-    finish_queue = Queue() # Если засунуть в очередь True - то перезагрузка, если False - то выход из программы
-    obrab_komand_thread = threading.Thread(target=obrab_komand, kwargs={
-                                           'in_q': cmd_queue, 'out_q': reply_queue, 'fin_q': finish_queue})
-    obrab_komand_thread.start()
-    if DEBUG:
-        cmd_queue.put(kmd_examples['cmd_kill'])
-        cmd_queue.put(kmd_examples['cmd_exit'])
-    obrab_komand_thread.join()
-
-
 def obrab_komand(in_q: Queue, out_q: Queue, fin_q: Queue) -> None:
-
-    def cmd_kill():
-        try:
-            ps_list = cmd['ps']
-            for psname in ps_list:
-                ls = []
-                for p in psutil.process_iter(['name']):
-                    if p.info['name'] == psname:
-                        ls.append(p)
-                for p in ls:                
-                    p.terminate()
-        except KeyError as ke:
-            put_error('Несуществующее поле: ' + ke.args[0])
-        except psutil.AccessDenied as ad:
-            put_error('Недостаточно полномочий для завершения процесса ' + ad.name)
-        except psutil.Error as err:
-            put_error(err.msg)
-
-    def cmd_start():
-        pass
-
-    def cmd_reboot():
-        fin_q.put(True)
-        reply_msg['result'] = True
-
-    def cmd_exit():
-        fin_q.put(False)
-        reply_msg['result'] = True
-
-    def cmd_test():
-        reply_msg['result'] = True
 
     def put_error(msg):
         reply_msg['result'] = False
         reply_msg['text'] = msg
 
-    selector = {
-        'kill': cmd_kill,
-        'start': cmd_start,
-        'reboot': cmd_reboot,
-        'exit': cmd_exit,
-        'test': cmd_test
-    }
-    
+    def kill_list(pslist, msg):
+        for p in pslist:
+            p.terminate()
+        put_error(msg)
+
     while True:
         cmd = in_q.get()
         reply_msg = {}
-
         try:
             reply_msg['id'] = cmd['id']
-            action = cmd['do']
-            selector[action]()
+            match cmd['do']:
+                case 'kill':
+                    try:
+                        for psname in cmd['ps']:
+                            for p in psutil.process_iter(['name']):
+                                if p.info['name'] == psname:
+                                    p.terminate()
+                    except psutil.AccessDenied as ad:
+                        put_error(
+                            'Недостаточно полномочий для завершения процесса ' + ad.name)
+                    except psutil.Error as err:
+                        put_error(err.msg)
+                    else:
+                        reply_msg['result'] = True
+                case 'start':
+                    pslist = []
+                    try:
+                        for p in cmd['ps']:
+                            env = os.environ.copy()
+                            try:
+                                my_env = p['env']
+                            except KeyError:
+                                pass
+                            else:
+                                for key in my_env:
+                                    env[key] = my_env[key]
+                            try:
+                                cwd = p['cwd']
+                            except KeyError:
+                                cwd = None
+                            pslist.append(subprocess.Popen(
+                                p['cmd'], env=env, cwd=cwd))
+                    except KeyError as ke:
+                        kill_list(pslist,
+                                  'Ошибка при запуске процесса. Несуществующее поле: ' + ke.args[0])
+                    except OSError as e:
+                        kill_list(pslist, 'Ошибка при запуске процесса. ' +
+                                  e.strerror+' '+e.filename)
+                    except ValueError as e:
+                        kill_list(pslist,
+                                  'Ошибка при запуске процесса. Некорректный аргумент: ' + e.args[0])
+                    else:
+                        reply_msg['result'] = True
+                case 'reboot':
+                    fin_q.put(True)
+                    reply_msg['result'] = True
+                case 'exit':
+                    fin_q.put(False)
+                    reply_msg['result'] = True
+                case 'test':
+                    reply_msg['result'] = True
+                case _:
+                    raise KeyError(cmd['do'])
         except KeyError as ke:
-            put_error('Несуществующее поле: ' + ke.args[0])            
+            put_error('Несуществующее поле: ' + ke.args[0])
         finally:
-            out_q.put(reply_msg)        
-
+            out_q.put(reply_msg)
         if not fin_q.empty():
             break
+
+
+def glavnaya():
+    cmd_queue = Queue()
+    reply_queue = Queue()
+
+    # Если засунуть в finish_queue True - то перезагрузка, если False - то выход из программы
+    finish_queue = Queue()
+    obrab_komand_thread = threading.Thread(target=obrab_komand, kwargs={
+                                           'in_q': cmd_queue,
+                                           'out_q': reply_queue,
+                                           'fin_q': finish_queue})
+    obrab_komand_thread.start()
+    if DEBUG:
+        cmd_queue.put(kmd_examples['cmd_kill'])
+        cmd_queue.put(kmd_examples['cmd_start'])
+        cmd_queue.put(kmd_examples['cmd_test'])
+        cmd_queue.put(kmd_examples['cmd_exit'])
+    obrab_komand_thread.join()
+    if DEBUG:
+        while not reply_queue.empty():
+            print(reply_queue.get())
+        while not finish_queue.empty():
+            print(finish_queue.get())
 
 
 if __name__ == '__main__':
     if DEBUG:
         for kmd in kmd_examples:
-            try:
-                json.dump(kmd_examples[kmd],
-                          open(kmd + '.json', 'w'),
-                          ensure_ascii=False,
-                          indent=4)
-            except KeyError as ke:
-                print('KeyError args: ' + ke.args)
-                raise
+            json.dump(kmd_examples[kmd],
+                      open(kmd + '.json', 'w'),
+                      ensure_ascii=False,
+                      indent=4)
     glavnaya()
